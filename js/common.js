ff81a2596d434fc4a81381557062d4e1e9d62672
aaabinbin <1910919177@qq.com>
2015-03-21 23:45:03 +0800

    add circle

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-	if(window.devicePixelRatio>1.25){
+	if(2.5>window.devicePixelRatio>1.25){
         $('img').each(function(){
         	var src = $(this).attr("src").split(".");
         	var newSrc = src[0]+"@2x."+src[1];
@@ -10,6 +10,15 @@ $(document).ready(function(){
 
         $(".per50").css({"width":"50%","height":"50%"});
     }
+
+    if(window.devicePixelRatio>2.5){
+        $('img').each(function(){
+        	var src = $(this).attr("src").split(".");
+        	var newSrc = src[0]+"@3x."+src[1];
+        	$(this).attr('src',newSrc);
+        });
+    }
+
     var u = navigator.userAgent, app = navigator.appVersion;
 	var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
 	var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
